a998a4f26dcfd1d444c1c26d732f1e76a36d7e6e
Harrison Powers <harrisonpowers@gmail.com>
2019-04-30 14:37:50 -0400

    Errors and formatting (#4)
    * Handle revtimes errors
    
    * Add html meta refresh
    
    * Add favicon
    
    * Add page title

--- whatsup.js
+++ whatsup.js
@@ -1,5 +1,8 @@
 const handleError = require('./lib/handle-error');
 const aws = require('./lib/aws');
+const revtimes = require('./lib/revtimes');
+
+const favicon = 'https://github.githubassets.com/images/icons/emoji/shipit.png';
 
 module.exports = (req, res) => {
   return whatsup(req,res)
@@ -11,10 +14,18 @@ async function whatsup(req, res) {
   // build html
   html = `
     <html>
+    <head>
+      <title>Whats up!</title>
+      <meta http-equiv="refresh" content="30">
+      <link rel="shortcut icon" href="${favicon}">
+      <link rel="apple-touch-icon" href="${favicon}">
+    </head>
     <body>
-    whats up!
-    <br><br>
-    ${ await aws.toString() }
+      whats up!
+      <br><br>
+      ${ await revtimes.toString() }
+      <br><br>
+      ${ await aws.toString() }
     </body>
     </html>
   `;
